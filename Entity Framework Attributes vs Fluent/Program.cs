b9d78548a3c1efcf6fc9497b121d0f0168861b2e
Kulpot <kulpotwahu@gmail.com>
2023-09-19 21:12:28 +0800

    .....

--- Entity Framework Attributes vs Fluent/Program.cs
+++ Entity Framework Attributes vs Fluent/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
@@ -79,6 +80,7 @@ class Video
     public int ID { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    [Required]
     public PlayList MyList { get; set; }
     
     
